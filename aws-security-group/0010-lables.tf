50221f8b8cb868edc5a9b0b613c79119f927ff44
Matkovskiy Aleksandr <amatkovskiy@provectus.com>
2019-11-05 11:00:38 +0200

    updated tag

--- aws-security-group/0010-lables.tf
+++ aws-security-group/0010-lables.tf
@@ -1,5 +1,5 @@
 module "security-group_label" {
-  source     = "git::https://github.com/matkovskiy/tf-modules.git?ref=tags/v0.0.1"
+  source     = "git::https://github.com/matkovskiy/tf-modules.git//tf-label?ref=tags/v0.0.3"
   namespace  = var.namespace
   name       = var.name
   stage      = var.stage
--- aws-subnet/0100-subnet-private.tf
+++ aws-subnet/0100-subnet-private.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "private_label" {
-  source     = "git::https://github.com/matkovskiy/tf-modules.git?ref=tags/v0.0.1"
+  source     = "git::https://github.com/matkovskiy/tf-modules.git//tf-label?ref=tags/v0.0.3"
   namespace  = var.namespace
   name       = var.name
   stage      = var.stage
