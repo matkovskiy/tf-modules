ea6d9f5b09170d1353509b9c489cbf65b18b8f63
Matkovskiy Aleksandr <amatkovskiy@provectus.com>
2019-11-07 12:13:39 +0200

    #some fixs

--- aws-rds/0400-outputs.tf
+++ aws-rds/0400-outputs.tf
@@ -1,24 +1,44 @@
-output "bucket_domain_name" {
-  value       = var.enabled ? join("", aws_s3_bucket.default.*.bucket_domain_name) : ""
-  description = "FQDN of bucket"
+output "instance_id" {
+  value       = join("", aws_db_instance.default.*.id)
+  description = "ID of the instance"
 }
 
-output "bucket_id" {
-  value       = var.enabled ? join("", aws_s3_bucket.default.*.id) : ""
-  description = "Bucket Name (aka ID)"
+output "instance_arn" {
+  value       = join("", aws_db_instance.default.*.arn)
+  description = "ARN of the instance"
 }
 
-output "bucket_arn" {
-  value       = var.enabled ? join("", aws_s3_bucket.default.*.arn) : ""
-  description = "Bucket ARN"
+output "instance_address" {
+  value       = join("", aws_db_instance.default.*.address)
+  description = "Address of the instance"
 }
 
-output "prefix" {
-  value       = var.lifecycle_prefix
-  description = "Prefix configured for lifecycle rules"
+output "instance_endpoint" {
+  value       = join("", aws_db_instance.default.*.endpoint)
+  description = "DNS Endpoint of the instance"
 }
 
-output "enabled" {
-  value       = var.enabled
-  description = "Is module enabled"
+output "subnet_group_id" {
+  value       = join("", aws_db_subnet_group.default.*.id)
+  description = "ID of the Subnet Group"
+}
+
+output "security_group_id" {
+  value       = join("", aws_security_group.default.*.id)
+  description = "ID of the Security Group"
+}
+
+output "parameter_group_id" {
+  value       = join("", aws_db_parameter_group.default.*.id)
+  description = "ID of the Parameter Group"
+}
+
+output "option_group_id" {
+  value       = join("", aws_db_option_group.default.*.id)
+  description = "ID of the Option Group"
+}
+
+output "hostname" {
+  value       = module.dns_host_name.hostname
+  description = "DNS host name of the instance"
 }
