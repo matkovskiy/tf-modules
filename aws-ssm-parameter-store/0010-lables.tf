7b4dbb302ac0455464f47a2e67893fba8c0c464a
Matkovskiy Aleksandr <amatkovskiy@provectus.com>
2019-11-07 17:57:55 +0200

    #some fixs

--- aws-ssm-parameter-store/0010-lables.tf
+++
@@ -1,9 +0,0 @@
-module "ssm-store_label" {
-  source     = "git::https://github.com/matkovskiy/tf-modules.git//tf-label?ref=tags/v0.0.3"
-  namespace  = var.namespace
-  name       = var.name
-  stage      = var.stage
-  delimiter  = var.delimiter
-  tags       = var.tags
-  enabled    = var.enabled
-}
