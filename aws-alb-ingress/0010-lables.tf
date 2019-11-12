042cac24d17aac8014c5ed1975a4c2f3a9455099
Matkovskiy Aleksandr <amatkovskiy@provectus.com>
2019-11-12 13:56:43 +0200

    #some fixs

--- aws-alb-ingress/0010-lables.tf
+++ aws-alb-ingress/0010-lables.tf
@@ -1,4 +1,4 @@
-amodule "default_label" {
+module "default_label" {
   enabled    = var.default_target_group_enabled
   source     = "git::https://github.com/matkovskiy/tf-modules.git//tf-label?ref=tags/v0.0.3"
   attributes = var.attributes
