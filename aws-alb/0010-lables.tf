7037210005d1f7122b19ce7f5e2d5db2a0bcefab
Matkovskiy Aleksandr <amatkovskiy@provectus.com>
2019-11-13 12:41:20 +0200

    #some fixs

--- aws-alb/0010-lables.tf
+++ aws-alb/0010-lables.tf
@@ -13,7 +13,7 @@ module "default_target_group_label" {
   source     = "git::https://github.com/matkovskiy/tf-modules.git//tf-label?ref=tags/v0.0.3"
   attributes = concat(var.attributes, ["default"])
   delimiter  = var.delimiter
-  name       = "TG-${var.name}"
+  name       = var.name
   namespace  = var.namespace
   stage      = var.stage
   tags       = var.tags
