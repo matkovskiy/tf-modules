59a5b1568cd2dbfc3e37eb4eca2cd209dd72f70a
Matkovskiy Aleksandr <amatkovskiy@provectus.com>
2019-11-21 10:29:35 +0200

    merged from master

---
+++ aws-ecr/0201-vars.tf
@@ -0,0 +1,75 @@
+variable "namespace" {
+  type        = string
+  description = "Namespace (e.g. `eg` or `cp`)"
+  default     = ""
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  default     = ""
+}
+
+variable "name" {
+  type        = string
+  description = "The Name of the application or solution  (e.g. `bastion` or `portal`)"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`)"
+}
+
+variable "enabled" {
+  type        = bool
+  description = "Set to false to prevent the module from creating any resources"
+  default     = true
+}
+
+variable "use_fullname" {
+  type        = bool
+  default     = true
+  description = "Set 'true' to use `namespace-stage-name` for ecr repository name, else `name`"
+}
+
+variable "principals_full_access" {
+  type        = list(string)
+  description = "Principal ARNs to provide with full access to the ECR"
+  default     = []
+}
+
+variable "principals_readonly_access" {
+  type        = list(string)
+  description = "Principal ARNs to provide with readonly access to the ECR"
+  default     = []
+}
+
+variable "scan_images_on_push" {
+  type        = bool
+  description = "Indicates whether images are scanned after being pushed to the repository (true) or not (false)"
+  default     = false
+}
+
+variable "max_image_count" {
+  description = "How many Docker Image versions AWS ECR will store"
+  default     = 500
+}
+
+variable "regex_replace_chars" {
+  type        = string
+  default     = "/[^a-zA-Z0-9-]/"
+  description = "Regex to replace chars with empty string in `namespace`, `environment`, `stage` and `name`. By default only hyphens, letters and digits are allowed, all other chars are removed"
+}
