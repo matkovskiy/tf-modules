1e2bba2d19ac803f6855d03b7cdbc9a8dabc11ec
Matkovskiy Aleksandr <amatkovskiy>
2020-01-29 20:35:03 +0200

    aws-iam-policy-document-aggregator

--- aws-cloudwatch-logs/0400-outputs.tf
+++ aws-cloudwatch-logs/0400-outputs.tf
@@ -1,16 +1,16 @@
 output "log_group_arn" {
-  value       = "${join("", aws_cloudwatch_log_group.default.*.arn)}"
+  value       = join("", aws_cloudwatch_log_group.default.*.arn)
   description = "ARN of the log group"
 }
 
 output "stream_arns" {
-  value       = ["${aws_cloudwatch_log_stream.default.*.arn}"]
+  value       = [aws_cloudwatch_log_stream.default.*.arn]
   description = "ARN of the log stream"
 }
 
 output "log_group_name" {
   description = "Name of log group"
-  value       = "${join("", aws_cloudwatch_log_group.default.*.name)}"
+  value       = join("", aws_cloudwatch_log_group.default.*.name)
 }
 # 
 # output "role_arn" {
