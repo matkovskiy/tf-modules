1c50a6bf934c73b068963ae482ee0f5365ddc2b8
Aleksandr Matkovskiy <matkovskiy.aleksandr>
2021-02-21 13:21:58 +0200

    new version

--- aws-ec2-instance/0212-sg.tf
+++ aws-ec2-instance/0212-sg.tf
@@ -1,5 +1,5 @@
 module "default_sg" {
-  source = "git::https://github.com/matkovskiy/tf-modules.git//aws-security-group?ref=tags/0.0.25"
+  source = "git::https://github.com/matkovskiy/tf-modules.git//aws-security-group?ref=tags/0.0.26"
   
   rules   = var.security_group_rules
   vpc_id  = var.vpc_id
